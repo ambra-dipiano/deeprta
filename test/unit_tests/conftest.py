d2fd4a7c9dd778e2c2557e219c3871597f8788d1
Ambra Di Piano <ambra.dipiano@inaf.it>
2023-02-06 10:09:55 +0100

    empty conftest for pytest

---
+++ test/unit_tests/conftest.py
@@ -0,0 +1,11 @@
+# *****************************************************************************
+# Copyright (C) 2023 INAF
+# This software is distributed under the terms of the BSD-3-Clause license
+#
+# Authors:
+# Ambra Di Piano <ambra.dipiano@inaf.it>
+# *****************************************************************************
+
+import os
+import pytest
+import logging
